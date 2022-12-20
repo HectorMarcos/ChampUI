358d83d8375a7c2f61c6739c47304ccf0cfc9c56
Héctor Marcos <hectormarcx@gmail.com>
2022-12-20 21:44:16 +0100

    first

---
+++ _retail_/Interface/Addons/SharedMedia_MyMedia/MyMedia.lua
@@ -0,0 +1,53 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "Poppins-Bold", [[Interface\Addons\SharedMedia_MyMedia\font\Poppins-Bold.ttf]]) 
+LSM:Register("font", "Poppins-SemiBold", [[Interface\Addons\SharedMedia_MyMedia\font\Poppins-SemiBold.ttf]]) 
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register("statusbar", "ElvUI A", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI A.tga]]) 
+LSM:Register("statusbar", "ElvUI B", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI B.tga]]) 
+LSM:Register("statusbar", "ElvUI C", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI C.tga]]) 
+LSM:Register("statusbar", "ElvUI D", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI D.tga]]) 
+LSM:Register("statusbar", "ElvUI E", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI E.tga]]) 
+LSM:Register("statusbar", "ElvUI F", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI F.tga]]) 
+LSM:Register("statusbar", "ElvUI G", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI G.tga]]) 
+LSM:Register("statusbar", "ElvUI H", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI H.tga]]) 
+LSM:Register("statusbar", "ElvUI I", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI I.tga]]) 
+LSM:Register("statusbar", "ElvUI J", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI J.tga]]) 
+LSM:Register("statusbar", "ElvUI K", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI K.tga]]) 
+LSM:Register("statusbar", "ElvUI L", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI L.tga]]) 
+LSM:Register("statusbar", "ElvUI M", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI M.tga]]) 
+LSM:Register("statusbar", "ElvUI N", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI N.tga]]) 
+LSM:Register("statusbar", "ElvUI O", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI O.tga]]) 
+LSM:Register("statusbar", "ElvUI P", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI P.tga]]) 
+LSM:Register("statusbar", "ElvUI Q", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI Q.tga]]) 
+LSM:Register("statusbar", "ElvUI R", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI R.tga]]) 
+LSM:Register("statusbar", "KlixPlugins", [[Interface\Addons\SharedMedia_MyMedia\statusbar\KlixPlugins.tga]]) 
+LSM:Register("statusbar", "REEEE", [[Interface\Addons\SharedMedia_MyMedia\statusbar\REEEE.tga]]) 
+LSM:Register("statusbar", "Skullflower", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Skullflower.tga]]) 
+LSM:Register("statusbar", "Skullflower1", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Skullflower1.tga]]) 
+LSM:Register("statusbar", "Skullflower2", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Skullflower2.tga]]) 
+LSM:Register("statusbar", "Skullflower3", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Skullflower3.tga]]) 
+LSM:Register("statusbar", "Skullflower4", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Skullflower4.tga]]) 
+LSM:Register("statusbar", "Skullflower5", [[Interface\Addons\SharedMedia_MyMedia\statusbar\Skullflower5.tga]]) 
+LSM:Register("statusbar", "SkullflowerGradient", [[Interface\Addons\SharedMedia_MyMedia\statusbar\SkullflowerGradient.tga]]) 
+LSM:Register("statusbar", "SkullflowerGradient1", [[Interface\Addons\SharedMedia_MyMedia\statusbar\SkullflowerGradient1.tga]]) 
+LSM:Register("statusbar", "SkullflowerLight", [[Interface\Addons\SharedMedia_MyMedia\statusbar\SkullflowerLight.tga]]) 
+LSM:Register("statusbar", "SkullflowerNeon", [[Interface\Addons\SharedMedia_MyMedia\statusbar\SkullflowerNeon.tga]]) 
