54f0726057b502e5b5c93dda9e786aaecc3a71f8
Héctor Marcos <hectormarcx@gmail.com>
2022-12-22 23:03:17 +0100

    rename

--- files/ChampUI/media/sharedmedia.lua
+++ files/ChampUI/media/sharedmedia.lua
@@ -2,4 +2,4 @@ local LSM = LibStub("LibSharedMedia-3.0")
 
 LSM:Register("font", "Poppins-Bold", [[Interface\Addons\ChampUI\media\Poppins-Bold.ttf]]) 
 LSM:Register("font", "Poppins-SemiBold", [[Interface\Addons\ChampUI\media\Poppins-SemiBold.ttf]]) 
-LSM:Register("statusbar", "ElvUI I", [[Interface\Addons\ChampUI\media\CUI.tga]]) 
+LSM:Register("statusbar", "ElvUI I", [[Interface\Addons\ChampUI\media\ElvUI I.tga]]) 
