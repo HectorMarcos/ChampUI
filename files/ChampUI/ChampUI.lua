583c35629119da3aad7b6e5e942a814caaf77110
Héctor Marcos <hectormarcx@gmail.com>
2022-12-22 17:14:24 +0100

    stable

---
+++ files/ChampUI/ChampUI.lua
@@ -0,0 +1,24 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "Poppins-Bold", [[Interface\Addons\SharedMedia_MyMedia\font\Poppins-Bold.ttf]]) 
+LSM:Register("font", "Poppins-SemiBold", [[Interface\Addons\SharedMedia_MyMedia\font\Poppins-SemiBold.ttf]]) 
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register("statusbar", "ElvUI I", [[Interface\Addons\SharedMedia_MyMedia\statusbar\ElvUI I.tga]]) 
