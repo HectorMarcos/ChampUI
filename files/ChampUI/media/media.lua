4efb752537c6a4f642e970dbf169e20ee56d36f0
HectorMarcos <hectormarcx@gmail.com>
2022-12-22 16:03:31 +0100

    clear to release a stable version

--- files/ChampUI/media/media.lua
+++
@@ -1,5 +0,0 @@
-local LSM = LibStub("LibSharedMedia-3.0") 
-
-LSM:Register("font", "Poppins-Bold", [[Interface\Addons\ChampUI\media\Poppins-Bold.ttf]]) 
-LSM:Register("font", "Poppins-SemiBold", [[Interface\Addons\ChampUI\media\Poppins-SemiBold.ttf]]) 
-LSM:Register("statusbar", "ElvUI I", [[Interface\Addons\ChampUI\media\CUI.tga]]) 
